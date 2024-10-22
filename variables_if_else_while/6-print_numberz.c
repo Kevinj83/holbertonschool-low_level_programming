5e67193f35e8c4e756307660b9ef672b04d7a788
kevinj83 <kevinjbr17@gmail.com>
2024-10-22 17:52:04 -0400

    count numbers again

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single-digit numbers from 0 to 9
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        putchar(i + '0'); // Convert the integer to its corresponding character
+    }
+
+    putchar('\n'); // Print a newline character
+
+    return (0);
+}
