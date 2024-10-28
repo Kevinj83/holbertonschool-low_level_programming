8688a43bc2ee3f408c53a7585ce397100d58f655
kevinj83 <kevinjbr17@gmail.com>
2024-10-28 13:16:09 -0400

    checks for digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * _isdigit(int c) - check if the character is a decimal
+ * @c : character to be checked
+ * Return - 1 if c is a digit , 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 0 , c >= 9 )
+}
