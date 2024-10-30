054372a734743439a989a3ac1d0a37d15b3e01f0
kevinj83 <kevinjbr17@gmail.com>
2024-10-30 10:25:54 -0400

    update the value

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
-/*
- * reset_to_98 - update the value of the variable
- * the pointer point to to 98
+/**
+ * reset_to_98 - updates the value of the variable
+ * the pointer points to to 98
  * @n: pointer to the variable to update
  */
-
-void reset_to_98(int *n);
-
+void reset_to_98(int *n)
 {
-*n = 98;
+	*n = 98;
 }
