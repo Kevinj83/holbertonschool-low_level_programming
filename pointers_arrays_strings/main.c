a877cf199bde9209729d9ab5704ac12d8ecb755c
kevinj83 <kevinjbr17@gmail.com>
2024-10-30 10:14:52 -0400

    update main

--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -1,3 +1,6 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,3 +13,5 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 int _putchar(char c);
+
+#endif
