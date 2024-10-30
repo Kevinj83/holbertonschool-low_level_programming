fc04c8895f3dee2d2a326f3d2ba9c65bfd60fbb0
kevinj83 <kevinjbr17@gmail.com>
2024-10-30 10:57:26 -0400

    return the length of a string

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -3,15 +3,19 @@
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
+ *
  * Return: the length of the string
  */
-int _strlen(char *s);
+int _strlen(char *s)
 {
-	char *str;
-	int len;
+	int i;
+	
+	i = 0;
 
-	str = "My first strlen!";
-	len = _strlen(str);
-	printf("%d\n", len);
-	return (0);
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
 }
