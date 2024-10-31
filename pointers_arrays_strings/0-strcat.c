ad3b2b3041c60e6c6739a40554b6857e0fd80203
kevinj83 <kevinjbr17@gmail.com>
2024-10-31 10:47:12 -0400

    contenates two strings

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * _strcat - concatenate two strings.
+ * _strcat - concatenate two strings
  * @dest: string to append to
  * @src: string to add
+ *
  * Return: a pointer to the resulting string
  */
 char *_strcat(char *dest, char *src);
