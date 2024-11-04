1a8144490c4ecde395e58314c29c0df5206d695a
kevinj83 <kevinjbr17@gmail.com>
2024-11-04 14:01:37 -0400

    copies memory area

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -3,14 +3,14 @@
 /**
  * *_memcpy - copies memory area
  * @dest: destination memory area
- * @src: memory  area to copy from
+ * @src: memory area to copy from
  * @n: number of bytes to copy
  *
  * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i:
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
