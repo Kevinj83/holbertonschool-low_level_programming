c27539f305245e506efb205a9d85dfcdbe3a8f63
kevinj83 <kevinjbr17@gmail.com>
2024-11-04 13:54:45 -0400

    copies memory area

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,22 +1,21 @@
 #include "main.h"
-#include <studio.h>
 
 /**
- * _memset - fills memory whit a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- * @n: number of times to copy b
+ * *_memcpy - copies memory area
+ * @dest: destination memory area
+ * @src: memory  area to copy from
+ * @n: number of bytes to copy 
  *
- * Return: pointer to the memory area s
+ * Return: pointer to dest
  */
-char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i:
 
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
+		for (i = 0; i < n; i++)
+		{
+			dest[i] = src[i];
+		}
 
-	return (s);
+	return (dest);
 }
